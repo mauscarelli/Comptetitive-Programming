6a254b21a28d318125488718227644523138d1fe
RodrigoRossetti <aracknock@gmail.com>
2020-07-30 08:29:48 -0300

    codeforces 977f

--- codeforces-977f.cpp
+++ codeforces-977f.cpp
@@ -4,9 +4,9 @@ using namespace std;
 #define endl '\n'
 
 void lics(vector<int> &v, int n) {
-	vector<int> dp(n), anterior(n);
+    vector<int> dp(n), anterior(n);
     map<int, int> indice;
-	int maior = INT_MIN, imax = 0;
+    int maior = INT_MIN, imax = 0;
     for (int i = 0; i < n; i++) {
         dp[i] = 1;
         anterior[i] = -1;
@@ -15,7 +15,7 @@ void lics(vector<int> &v, int n) {
             dp[i] = dp[indice[v[i] - 1]] + 1;
         }
         indice[v[i]] = i;
-		if (dp[i] > maior) maior = dp[i], imax = i;
+        if (dp[i] > maior) maior = dp[i], imax = i;
     }
     vector<int> r = {imax};
     while (anterior[imax] != -1) {
@@ -23,19 +23,19 @@ void lics(vector<int> &v, int n) {
         r.push_back(imax);
     }
     reverse(r.begin(), r.end());
-	cout << maior << endl;
+    cout << maior << endl;
     for (int &x : r)
         cout << x + 1 << " ";
     cout << endl;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	int n;
-	cin >> n;
-	vector<int> v(n);
-	for (auto &x : v) cin >> x;
-	lics(v, n);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &x : v) cin >> x;
+    lics(v, n);
 	return 0;
 }
